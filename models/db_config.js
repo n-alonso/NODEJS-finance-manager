b814fee9bd2942092ea6d4676d367f5f4edd641b
Nicolas Alonso <63936366+n-alonso@users.noreply.github.com>
2022-07-18 11:06:10 +0300

    Update db_config.js

--- models/db_config.js
+++ models/db_config.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+    connectionString: process.env.DATABASE_URL,	//use DATABASE_URL environment variable from Heroku app 
     ssl: {
       rejectUnauthorized: false // don't check for SSL cert
     }
